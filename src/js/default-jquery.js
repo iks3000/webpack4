e02cc89da5b89eacdf5baddf58efceb59fdccd8d
iks3000 <khomyn@gmail.com>
2018-08-31 16:05:37 +0300

    add timer start stop reset

--- src/js/default-jquery.js
+++ src/js/default-jquery.js
@@ -1,9 +1,50 @@
 import $ from "jquery";
 
-const app = (a, b) => {
+const app = (a,b) => {
   return a * b;
 };
 
+'use strict';
+
 $(function() {
-  $('#demo').text(app(6,7));
+
+  var timer = null,
+      interval = 1000,
+      counter = app(6,7),
+      number = $('#demo');
+
+  number.text(counter);
+
+  $('.google').on('click', startTimer);
+  $('.apple').on('click', stopTimer);
+  $('.webpack').on('click', resetTimer);
+
+  function startTimer() {
+    if (timer !== null) return;
+    timer = setInterval(function() {
+      counter++;
+      number.text(counter);
+
+      // if (counter < 0) {
+      //   stopTimer();
+      //   number.text("time's Up");
+      // }
+    }, interval);
+
+  }
+
+  function stopTimer() {
+    clearInterval(timer);
+    timer = null;
+  }
+
+  function resetTimer() {
+    stopTimer();
+    counter = app(6,7);
+    number.text(counter);
+  }
+
+startTimer();  // auto start
+
+
 });
